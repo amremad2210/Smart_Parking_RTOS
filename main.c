026a6b171f39709194964c541c33058018c46165
amremad <amremad2210@gmail.com>
2026-05-04 22:41:27 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "TM4C123GH6PM.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include <basic_io.h>
+#include <stdio.h>
+
+
+
+
+
+int main(void)
+{
+	
+	while(1){
+	
+		
+		
+	}
+	
+
+}
+
+
